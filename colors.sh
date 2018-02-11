87c0317161b212f760f7b5930a8a8ae536599671
johe_PC-Fixe <anon@ramajhe.fr>
2018-02-11 11:14:53 +0100

    Ajout colors.sh

---
+++ colors.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Définition des couleurs / modes :
+# Copié sur : https://misc.flogisoft.com/bash/tip_colors_and_formatting
+RED="\e[31m"
+BLUE="\e[34m"
+GREEN="\e[32m"
+BOLD="\e[1m"
+LIGHTGREEN="\e[92m"
+NORMAL="\e[0m"
+
+echo -e "This is ${BLUE}Blue and ${BOLD}bold.${NORMAL}That's it!"
+echo -e "This is ${GREEN}Green and ${BOLD}bold.${NORMAL}That's it!"
+
